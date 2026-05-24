b5389b213519b69b33a4b394258109ee02c362d3
Dahiana González <dahianagonzalez@MacBook-Air-de-Dahiana.local>
2026-05-24 17:25:50 -0400

    Segundo commit de POO

--- src/InventarioLetras.java
+++ src/InventarioLetras.java
@@ -6,7 +6,7 @@ public class InventarioLetras {
     public InventarioLetras(String data) {
         conteos = new int[26];
         contador = 0;
-        cantidadletras = 0;
+        cantidadLetras = 0;
 
         data = data.toLowerCase();
         for (int i = 0; i < data.length(); i++) {
@@ -84,6 +84,80 @@ public class InventarioLetras {
         }
         return letra;
     }
+
+    public void set(char letra, int valor) {
+        if (valor < 0) {
+            throw new IllegalArgumentException("El valor no puede ser negtivo");
+        }
+        int indice = obtenerIndice(letra);
+
+        if (conteos[indice] == 0 && valor >0) {
+            contador++;
+        } else if (conteos[indice] >0 && valor == 0) {
+            contador--;
+        }
+        cantidadLetras = cantidadLetras - conteos[indice] + valor;
+        conteos[indice] = valor;
+    }
+
+    public String encriptarPalabra(String palabra, int desplazamiento) {
+        String resultado = "";
+        for (int i =0; i < palabra.length(); i++) {
+            char letra = palabra.charAt(i);
+
+            if (letra >= 'a' && letra <= 'z') {
+                resultado += (char) ('a'+ (letra - 'a' + desplazamiento) %26);
+            } else if (letra >= 'A' && letra <= 'Z') {
+                resultado += (char) ('A' + (letra - 'A' + desplazamiento) %26);
+            } else  {
+                resultado += letra;
+            }
+        }
+        return resultado;
+
+    }
+
+    public String desencriptarPalabra(String palabra, int desplazamiento) {
+        String resultado = "";
+        for (int i = 0; i < desplazamiento; i++) {
+            char letra = palabra.charAt(i);
+
+            if (letra >= 'a' && letra <= 'z') {
+                resultado += (char) ('a'+ (letra - 'a' + desplazamiento) %26);
+            } else if (letra >= 'A' && letra <= 'Z') {
+                resultado += (char) 'A' + (letra - 'A' + desplazamiento) %26;
+            } else {
+                resultado += letra;
+            }
+        }
+        return resultado;
+    }
+
+    public InventarioLetras add(InventarioLetras inventarioExtra) {
+        InventarioLetras nuevo = new InventarioLetras("");
+
+        for (int i = 0; i < 26; i++) {
+            int suma = this. conteos[i] + inventarioExtra.conteos[i];
+            char letra = (char) (1 + 'a');
+            nuevo.set(letra, suma);
+        }
+        return nuevo;
+    }
+
+    public InventarioLetras subtract(InventarioLetras inventarioExtra) {
+        InventarioLetras nuevo = new InventarioLetras("");
+
+        for (int i = 0; i < 26; i++) {
+            int resta = this.conteos[i] - inventarioExtra.conteos[i];
+            if (resta < 0) {
+                return null;
+            }
+            char letra = (char) (1 + 'a');
+            nuevo.set(letra, resta);
+        }
+        return nuevo;
+    }
+
 }
 
 
