35f9353142245cceace8dc633818ca9182a9bc6a
Dahiana González <dahianagonzalez@MacBook-Air-de-Dahiana.local>
2026-05-25 20:31:22 -0400

    Commit final

--- src/InventarioLetras.java
+++ src/InventarioLetras.java
@@ -1,62 +1,88 @@
+// Proyecto N°1 de POO
+// Profesor: Luis Canales
+
+// Esta es una clase que guarda cuántas veces aparece cada letra del alfabeto inglés en un texto
+// Ignora números, puntuación, tildes y la ñ
+// Además, no distingue entre mayúsculas y minúsculas
+
+
 public class InventarioLetras {
+
+    // Arreglo de 26 posiciones que guarda la cantidad de veces que aparece cada letra
     private int[] conteos;
+
+    // Arreglo que guarda la suma total de las letras contadas
     private int contador;
+
+    // Arreglo que guarda cuantas letras diferentes tienen cantidad mayor que 0
     private int cantidadLetras;
 
+    // Constructor: recibe un texto y cuenta las letras que contiene del alfabeto inglés
     public InventarioLetras(String data) {
-        conteos = new int[26];
+        conteos = new int[26];  //inicializa los conteos en 0
         contador = 0;
         cantidadLetras = 0;
 
         data = data.toLowerCase();
+
+        // Recorre cada carácter del String que recibe
         for (int i = 0; i < data.length(); i++) {
             char letra = data.charAt(i);
 
+            // Solo contaremos las letras del alfabeto inglés
             if (letra >= 'a' && letra <= 'z') {
                 int indice = letra - 'a';
 
                 if (conteos[indice] == 0) {
                     cantidadLetras++;
                 }
-                conteos[indice]++;
-                contador++;
+                conteos[indice]++;   // suma 1 al conteo de esa letra
+                contador++;     // suma 1 al total general
             }
         }
     }
+    // Método que verifica si el carácter es válido dentro del alfabeto inglés
     private boolean letraValida(char letra) {
-        letra = Character.toLowerCase(letra);
+
+        letra = Character.toLowerCase(letra); // convierte todo a minúscula
+
         return (letra >= 'a' && letra <= 'z');
     }
 
+    // Método  que devuelve el índice en el arreglo para una letra dada
     private int obtenerIndice(char letra) {
         letra = Character.toLowerCase(letra);
 
         if (!letraValida(letra)) {
-            throw new IllegalArgumentException("Letra no valida");
+            throw new IllegalArgumentException("Letra no valida"); // lanza excepción si la letra no es válida
         }
         return letra - 'a';
     }
 
+    // Devuelve la cantidad total de letras guardadas en el inventario
     public int size() {
         return contador;
     }
 
+    // Devuelve verdadero si el inventario no tiene letras
     public boolean isEmpty() {
         return cantidadLetras == 0;
     }
 
+    // Devuelve cuantas veces aparece una letra en el inventario
     public int get(char letra) {
         int indice = obtenerIndice(letra);
         return conteos[indice];
     }
 
-    public String toSring() {
+    // Devuelve el inventario entre corchetes, en orden alfabético
+    public String toString() {
         String resultado = "[";
 
         for (int i = 0; i < conteos.length; i++) {
-            char letra = (char) (i + 'a');
+            char letra = (char) (i + 'a');   // obtiene la letra que corresponde a la posición i
 
-            for (int j =0; j < conteos[i]; j++) {
+            for (int j =0; j < conteos[i]; j++) {     // repite la letra las veces que indique su conteo
                 resultado += letra;
             }
         }
@@ -64,6 +90,7 @@ public class InventarioLetras {
         return resultado;
     }
 
+    // Encripta una letra usando el cifrado Cesar, anvanza 3 posiciones en el abecedario
     public char encriptarCesar(char letra) {
         if (letra >= 'a' && letra <= 'z') {
             return (char) ('a' + (letra - 'a'+3) %26);
@@ -74,6 +101,7 @@ public class InventarioLetras {
         return letra;
     }
 
+    // Desencripta una letra usando el cifrado Cesar inverso con desplazamiento de 3 posiciones en el abecedario
     public char desencriptarCesar(char letra){
         if (letra >= 'a' && letra <= 'z') {
             return (char) ('a' + (letra - 'a'- 3 + 26) %26);
@@ -85,6 +113,7 @@ public class InventarioLetras {
         return letra;
     }
 
+    // Cambia manualmente l
     public void set(char letra, int valor) {
         if (valor < 0) {
             throw new IllegalArgumentException("El valor no puede ser negtivo");
@@ -100,6 +129,7 @@ public class InventarioLetras {
         conteos[indice] = valor;
     }
 
+    // Encripta un palabra completa usando el desplazamiento
     public String encriptarPalabra(String palabra, int desplazamiento) {
         String resultado = "";
         for (int i =0; i < palabra.length(); i++) {
@@ -117,15 +147,19 @@ public class InventarioLetras {
 
     }
 
+    // Desencripta una palabra completa usando el desplazamiento
     public String desencriptarPalabra(String palabra, int desplazamiento) {
         String resultado = "";
-        for (int i = 0; i < desplazamiento; i++) {
+
+        // Recorre todos los caracteres de la palabra
+        for (int i = 0; i < palabra.length(); i++) {
             char letra = palabra.charAt(i);
 
             if (letra >= 'a' && letra <= 'z') {
-                resultado += (char) ('a'+ (letra - 'a' + desplazamiento) %26);
+                // Retrocede el desplazamiento, suma 26 para evitar negativo
+                resultado += (char) ('a'+ (letra - 'a' - desplazamiento) %26);
             } else if (letra >= 'A' && letra <= 'Z') {
-                resultado += (char) 'A' + (letra - 'A' + desplazamiento) %26;
+                resultado += (char) ('A' + (letra - 'A' - desplazamiento) %26);
             } else {
                 resultado += letra;
             }
@@ -133,26 +167,28 @@ public class InventarioLetras {
         return resultado;
     }
 
+    // Suma de este inventario junto a otro y devuelve un inventario nuevo
     public InventarioLetras add(InventarioLetras inventarioExtra) {
         InventarioLetras nuevo = new InventarioLetras("");
 
         for (int i = 0; i < 26; i++) {
-            int suma = this. conteos[i] + inventarioExtra.conteos[i];
-            char letra = (char) (1 + 'a');
+            int suma = this. conteos[i] + inventarioExtra.conteos[i];   //suma posición por posición
+            char letra = (char) (i + 'a');
             nuevo.set(letra, suma);
         }
         return nuevo;
     }
 
+    // Resta de otro inventario a este y devuelve un inventario nuevo
     public InventarioLetras subtract(InventarioLetras inventarioExtra) {
         InventarioLetras nuevo = new InventarioLetras("");
 
         for (int i = 0; i < 26; i++) {
             int resta = this.conteos[i] - inventarioExtra.conteos[i];
             if (resta < 0) {
-                return null;
+                return null;   // Retornara null si la cantidad es negativa
             }
-            char letra = (char) (1 + 'a');
+            char letra = (char) (i + 'a');
             nuevo.set(letra, resta);
         }
         return nuevo;
--- src/PruebaInventario.java
+++ src/PruebaInventario.java
@@ -2,38 +2,69 @@
 public class PruebaInventario {
     public static void main(String[] args) {
 
+        // Creamos un inventario con la frase "Hola Mundo"
         InventarioLetras inv = new InventarioLetras("Hola mundo");
+
+        // Prueba del método toString()
         System.out.println("Inventario de 'Hola Mundo': " + inv);
+
+        // Prueba del método size()
+        // "Hola Mundo" tiene 9 letras
         System.out.println("size: " + inv.size());
+
+        // Prueba del método isEmpty()
+        // Verdadero si el inventario tiene letras y Falso si contiene letras
         System.out.println("isEmpty: " + inv.isEmpty());
+
+        // Prueba del método get()
+        // La letra 'o' aparece 2 en "Hola Mundo"
         System.out.println("get('o'): " + inv.get('o'));
 
+        // Prueba de encriptarCesar()
+        // Con un desplazamiento de 3, la 'a' se transformara en 'd'
         System.out.println("encriptarCesar('a'): " + inv.encriptarCesar('a'));
-        System.out.println("desencriptarCesar('d': " + inv.desencriptarCesar('d'));
 
+        // Prueba de desencriptarCesar()
+        // En el sentido inverso, la 'd' vuelve a ser 'a'
+        System.out.println("desencriptarCesar('d'): " + inv.desencriptarCesar('d'));
+
+        // Prueba de set()
+        inv.set('a', 5);
         System.out.println("Después de set('a', 5): " + inv);
         System.out.println("get('a'): " + inv.get('a'));
 
+        // Hacemos dos inventarios nuevos
         InventarioLetras inv1 = new InventarioLetras("Alan Turing");
         InventarioLetras inv2 = new InventarioLetras("Ada Lovelace");
 
+        // Prueba de add()
+        // Sumara las cantidades de inv1 e inv2
         InventarioLetras suma = inv1.add(inv2);
         System.out.println("inv1: " + inv1);
         System.out.println("inv2: " + inv2);
         System.out.println("suma: " + suma);
 
 
+        // Probamos subtract
+        // suma - inv1 debe dar algo similar a inv2
         InventarioLetras resta = suma.subtract(inv1);
         System.out.println("suma - inv1: " + resta);
 
-        InventarioLetras restaLoca = suma.subtract(inv1);
+        // Prueba de un resta imposible (restaLoca)
+        // Si alguna cantidad queda negativa, retornara null
+        InventarioLetras restaLoca = inv1.subtract(suma);
         System.out.println("inv1 - suma: " + restaLoca);
 
+        // Manejo de excepciones en get()
+        // Dado que estamos usando el alfabeto inglés, la 'ñ' no corresponde y debe lanzarse error
         try {
             inv.get('ñ');
         } catch (IllegalArgumentException e) {
             System.out.println("Error: " + e.getMessage());
         }
+
+        // Manejo de excepciones en set()
+        // No se permiten cantidad negativas, por lo que debe lanzarse error
         try {
             inv.set('a', -3);
         } catch (IllegalArgumentException e) {
